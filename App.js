f3518d5ab88e5468236863021c26749b4c02bfd3
veitim <kenganrakentaja@gmail.com>
2024-09-20 16:33:59 +0300

    valmis

--- App.js
+++ App.js
@@ -1,20 +1,84 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import { useState } from 'react';
 
 export default function App() {
+
+  const apikey = process.env.EXPO_PUBLIC_API_KEY;
+
+  const [address, setAddress] = useState('');
+  const [location, setLocation] = useState({
+    latitude: 60.200692,
+    longitude: 24.934302,
+    latitudeDelta: 0.0322,
+    longitudeDelta: 0.0221
+  });
+
+  const getLocation = () => {
+    fetch(`https://geocode.maps.co/search?q=${address}&api_key=${apikey}`)
+    .then(response => {
+      if (!response.ok)
+        throw new Error("Error in fetch:" + response.statusText);
+      return response.json()
+    })
+    .then(data => {
+      setLocation({...location, latitude: Number(data[0].lat), longitude: Number(data[0].lon)})
+      //console.log(location)
+    })
+    .catch(err => console.error(err));
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <MapView
+        style={styles.map} 
+        region={location}
+      >
+      <Marker
+        coordinate={{
+          latitude: location.latitude,
+          longitude: location.longitude
+        }}
+        title={address} 
+        />
+      </MapView>
+      <TextInput
+        underlineColorAndroid={'gray'}
+        style={styles.input} 
+        placeholder='Place address' 
+        value={address}
+        onChangeText={text => setAddress(text)} 
+      />
+      <View style={styles.button}>
+        <Button title="Show" onPress={getLocation}/>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  map: {
+    width: '100%', 
+    height: '100%',
+    paddingBottom: 30,
+  },
   container: {
+    paddingTop: 70,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    height: 100,
+    width: '100%',
+  },
+  input: {
+    height: 40,
+    width: '100%',
+    borderColor: 'gray', 
+    borderBottomWidthborderWidth: 1,
   },
 });
